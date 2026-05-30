5f4cab378a4182e126b13541b3c4e96b1d903c7e
fractalizes <ibunsiri@ucsc.edu>
2026-05-30 01:12:39 -0700

    typing works with new input system!
    TODO: make it be able to detect words from given txt file

--- WordWhatever/Assets/Scripts/Input/ReadKeyboardInput.cs
+++ WordWhatever/Assets/Scripts/Input/ReadKeyboardInput.cs
@@ -1,33 +1,77 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ReadKeyboardInput : MonoBehaviour
 {
+    [Header("[== REFERENCES ==]")]
     [SerializeField] private TMP_Text canvasText;
 
+    [Header("[== SETTINGS ==]")]
+    [SerializeField] private float initialDelay = 0.5f;
+    [SerializeField] private float repeatDelay = 0.025f;
+
+    private InputActions inputActions;
     private bool submitted = false;
+    private bool backspaceHeld;
+    private float nextDeleteTime;
+
+    private void Awake() => inputActions = new InputActions();
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+        inputActions.Player.BackspaceKey.started += OnBackspaceStarted;
+        inputActions.Player.BackspaceKey.canceled += OnBackspaceCancelled;
+        inputActions.Player.EnterKey.performed += OnEnter;
+        if (Keyboard.current != null) Keyboard.current.onTextInput += OnTextInput;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.BackspaceKey.started -= OnBackspaceStarted;
+        inputActions.Player.BackspaceKey.canceled -= OnBackspaceCancelled;
+        inputActions.Player.EnterKey.performed -= OnEnter;
+        inputActions.Disable();
+        if (Keyboard.current != null) Keyboard.current.onTextInput -= OnTextInput;
+    }
+
+    private void OnBackspaceStarted(InputAction.CallbackContext context)
+    {
+        backspaceHeld = true;
+        // delete a character immediately
+        DeleteCharacter();
+        nextDeleteTime = Time.time + initialDelay;
+    }
+
+    private void OnBackspaceCancelled(InputAction.CallbackContext context) => backspaceHeld = false;
 
     private void Update()
     {
-        if (submitted) return;
-        UpdateText(); // borrowed from https://docs.unity3d.com/ScriptReference/Input-inputString.html
+        if (!backspaceHeld || submitted) return;
+        if (Time.time >= nextDeleteTime)
+        {
+            DeleteCharacter();
+            nextDeleteTime = Time.time + repeatDelay;
+        }
     }
 
-    private void UpdateText()
+    private void DeleteCharacter()
     {
-        foreach (char c in Input.inputString)
-        {
-            // if character is new line or return carriage
-            if (c == '\n' || c == '\r')
-            {
-                canvasText.color = Color.green;
-                submitted = true;
-            }
-            else
-            {
-                Debug.Log(canvasText.text.Length > 0);
-                canvasText.text += char.ToUpper(c);
-            }
-        }
+        if (canvasText.text.Length > 0) canvasText.text = canvasText.text[..^1];
+    }
+
+    private void OnTextInput(char c)
+    {
+        if (submitted) return;
+        if (c == '\b' || c == '\r' || c == '\n') return;
+        if (!char.IsLetter(c)) return; // we don't care about spaces, only letters!
+        canvasText.text += char.ToUpper(c);
+    }
+
+    private void OnEnter(InputAction.CallbackContext context)
+    {
+        submitted = true;
+        canvasText.color = Color.green;
     }
 }
