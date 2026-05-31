c1760a4cb16b889a792af311a2a88f3066fe735b
fractalizes <ibunsiri@ucsc.edu>
2026-05-31 05:11:59 -0700

    generatable possible vowels from dictionary
    TODO: link them into scene

--- WordWhatever/Assets/Scripts/ScoringSystem.cs
+++ WordWhatever/Assets/Scripts/ScoringSystem.cs
@@ -1,21 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class ScoringSystem : MonoBehaviour
 {
+    [Header("[== REFERENCES ==]")]
     [SerializeField] private TMP_Text scoreText;
     [SerializeField] private WordCheck wordChecker;
 
+    [Header("[== SETTINGS ==]")]
+    [SerializeField] private int defaultLetterCount = 2;
+
     private int score = 0;
+    private List<char> chosenLetters;
 
     public int Score => score;
+
+    private void Start() => UpdateChosenLetters(defaultLetterCount);
 
     public bool CalculateScore(string text)
     {
         bool valid = wordChecker.IsValidWord(text);
-        score += valid ? 1 : 0;
-        UpdateScoreText();
+        if (valid)
+        {
+            score++;
+            UpdateScoreText();
+            UpdateChosenLetters(ChooseLetterNumber(text));
+        }
         return valid;
+    }
+
+    private int ChooseLetterNumber(string word) => defaultLetterCount + (word.Length / 4);
+
+    private void UpdateChosenLetters(int letterCount)
+    {
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        List<char> possibleLetters = new List<char>();
+        while (true)
+        {
+            possibleLetters.Clear();
+            for (int i = 0; i < letterCount; i++)
+            {
+                int randInd = Random.Range(0, alphabet.Length);
+                possibleLetters.Add(alphabet[randInd]);
+            }
+            if (wordChecker.IsPossible(possibleLetters)) break;
+        }
+        chosenLetters = new List<char>(possibleLetters);
+        Debug.Log($"CHOSEN LETTERS: {string.Join("", chosenLetters)}");
     }
 
     private void UpdateScoreText() => scoreText.text = $"SCORE: {score}";
--- WordWhatever/Assets/Scripts/wordCheck.cs
+++ WordWhatever/Assets/Scripts/wordCheck.cs
@@ -7,12 +7,36 @@ public class WordCheck : MonoBehaviour
     [SerializeField] private TextAsset vocabularyList;
 
     private HashSet<string> validWords = new HashSet<string>();
-    private void Start() => LoadDictionary();
+    private void Awake() => LoadDictionary();
 
     private void LoadDictionary()
     {
         string[] lines = vocabularyList.text.Split('\n', System.StringSplitOptions.RemoveEmptyEntries);
         foreach (string line in lines) validWords.Add(line.Trim().ToLower());
+    }
+
+    public bool IsPossible(List<char> letters)
+    {
+        foreach (string word in validWords)
+        {
+            if (word.Length < letters.Count) continue; // skip words that are smaller than possible letters
+            if (ContainsLettersInOrder(word, letters)) return true;
+        }
+        return false;
+    }
+
+    private bool ContainsLettersInOrder(string word, List<char> letters)
+    {
+        int letterIndex = 0;
+        foreach (char c in word)
+        {
+            if (c == letters[letterIndex])
+            {
+                letterIndex++;
+                if (letterIndex == letters.Count) return true;
+            }
+        }
+        return false;
     }
 
     public bool IsValidWord(string guess) => validWords.Contains(guess.ToLower());
